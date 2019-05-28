427c2c0ebd65103a7f025e8718ceae0011b2840f
Nik Bamert <bamertn@ethz.ch>
2019-05-28 10:24:11 +0200

    63 classes

--- Middlewares/ST/AI/AI/include/network.h
+++ Middlewares/ST/AI/AI/include/network.h
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    network.h
   * @author  AST Embedded Analytics Research Platform
-  * @date    Mon May 27 19:35:22 2019
+  * @date    Tue May 28 10:23:36 2019
   * @brief   AI Tool Automatic Code Generator for Embedded NN computing
   ******************************************************************************
   * @attention
--- Middlewares/ST/AI/AI/src/network.c
+++ Middlewares/ST/AI/AI/src/network.c
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    network.c
   * @author  AST Embedded Analytics Research Platform
-  * @date    Mon May 27 19:35:22 2019
+  * @date    Tue May 28 10:23:36 2019
   * @brief   AI Tool Automatic Code Generator for Embedded NN computing
   ******************************************************************************
   * @attention
@@ -53,7 +53,7 @@
 #endif
 
 #undef AI_TOOLS_DATE_TIME
-#define AI_TOOLS_DATE_TIME   "Mon May 27 19:35:22 2019"
+#define AI_TOOLS_DATE_TIME   "Tue May 28 10:23:36 2019"
 
 #undef AI_TOOLS_COMPILE_TIME
 #define AI_TOOLS_COMPILE_TIME    __DATE__ " " __TIME__
